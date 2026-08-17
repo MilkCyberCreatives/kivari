5ce71dcffdb5c6024d8bfc5fb58c6eac85b2d8f9
Milk Cyber Creatives <milkcybercreatives@gmail.com>
2026-08-18 00:00:42 +0200

    Use documented catch-all header route

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: [
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
